9d3fd01f600faadd06cad6c4dfb98aef5d417d62
Furkan Karakus <furkankarakus01@hotmail.com>
2023-08-02 15:39:31 +0300

    Active DevTools

---
+++ src/main/java/com/furkankarakus/ui/mvc/utils/ConfigProgramming.java
@@ -0,0 +1,7 @@
+package com.furkankarakus.ui.mvc.utils;
+
+public class ConfigProgramming {
+    //DevTools ==> LiveReload
+    //file ==> settings ==> compiler ==> Build project automatically(OK clicked) (Ctrl+Alt+S)
+    // file ==> Advanced Settings ==>Allow Auto-make to start even (OK clicked)
+}
