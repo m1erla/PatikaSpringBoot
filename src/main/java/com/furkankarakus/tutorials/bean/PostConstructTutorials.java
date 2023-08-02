40b7872eb1d41aefb89cb6e68830c0224d6e305b
Furkan Karakus <furkankarakus01@hotmail.com>
2023-08-02 20:25:08 +0300

    revised

--- src/main/java/com/furkankarakus/tutorials/bean/PostConstructTutorials.java
+++ src/main/java/com/furkankarakus/tutorials/bean/PostConstructTutorials.java
@@ -7,23 +7,23 @@ import org.springframework.stereotype.Controller;
 import java.util.logging.Logger;
 @Controller
 public class PostConstructTutorials {
-    @Autowired
-    Logger LOG;
-   // because "this.LOG" is null
-//    public PostConstructTutorials(){
-//        LOG.info("Invoke Log info.");
+//    @Autowired
+//    Logger LOG;
+//   // because "this.LOG" is null
+////    public PostConstructTutorials(){
+////        LOG.info("Invoke Log info.");
+////    }
+//
+//    //bean henüz başlamadığından null  alıyoruz ve burada bağımlılığı enjecte edemiyoruz.
+//    //Görevi: Bir bean nesnesi oluşturulduğunda hemen oluşur.
+//
+//    @PostConstruct
+//    public void init(){
+//        LOG.info("Invoke Log info");
 //    }
-
-    //bean henüz başlamadığından null  alıyoruz ve burada bağımlılığı enjecte edemiyoruz.
-    //Görevi: Bir bean nesnesi oluşturulduğunda hemen oluşur.
-
-    @PostConstruct
-    public void init(){
-        LOG.info("Invoke Log info");
-    }
-
-    public static void main(String[] args) {
-        PostConstructTutorials postConstructTutorials = new PostConstructTutorials();
-        System.out.println(postConstructTutorials);
-    }
+//
+//    public static void main(String[] args) {
+//        PostConstructTutorials postConstructTutorials = new PostConstructTutorials();
+//        System.out.println(postConstructTutorials);
+//    }
 }
