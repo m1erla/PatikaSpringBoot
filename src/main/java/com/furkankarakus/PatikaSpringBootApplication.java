72946bea96fed9683200265851c15c2603c31530
Furkan Karakus <furkankarakus01@hotmail.com>
2023-08-02 14:17:16 +0300

    Initial Commit (patika)

---
+++ src/main/java/com/furkankarakus/PatikaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.furkankarakus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatikaSpringBootApplication.class, args);
+    }
+
+}
