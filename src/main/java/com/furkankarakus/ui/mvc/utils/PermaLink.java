6b5a356412d6ff666bfa3f79ebc74d4e66b9cd25
Furkan Karakus <furkankarakus01@hotmail.com>
2023-08-04 16:29:19 +0300

    revised

---
+++ src/main/java/com/furkankarakus/ui/mvc/utils/PermaLink.java
@@ -0,0 +1,9 @@
+package com.furkankarakus.ui.mvc.utils;
+
+public class PermaLink {
+    // Github : https://github.com/m1erla/PatikaSpringBoot
+    // SWAGGER : http://localhost:8080/swagger-ui/index.html
+    // H2-console : http://localhost:8080/h2-console/
+    // JSON online parser :https://jsoneditoronline.org/#left=local.gesomi&right=local.revibe
+    // Jwt ==> https://jwt.io/
+}
