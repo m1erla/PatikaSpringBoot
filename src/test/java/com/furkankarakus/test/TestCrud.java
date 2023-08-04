0c59c5f3155f16d002a4819c1b9bd5f4f2980c8e
Furkan Karakus <furkankarakus01@hotmail.com>
2023-08-04 20:03:30 +0300

    Unit Test (CRUD)

---
+++ src/test/java/com/furkankarakus/test/TestCrud.java
@@ -0,0 +1,14 @@
+package com.furkankarakus.test;
+
+public interface TestCrud {
+
+    // Test Crud
+
+    public void testCreate();
+    public void testUpdate();
+    public void testList();
+
+    public void testFindById();
+
+    public void testDelete();
+}
